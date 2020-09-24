e005b016f92a805acc1a7d933f1359c3bbe390f0
Radames Vaz <rgvtovar@hotmail.com>
2020-09-24 17:59:39 -0400

    agregando login

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,9 +5,10 @@ const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 
 //  @Descripcion    Registro de usuarios
-//  @Ruta y Metodo  POST api/v1/auth/register
+//  @Ruta y Metodo  POST api/v1/auth/registro
 //  @Acceso         Publica
 exports.register = asyncHandler(async (req, res, next) => {
+
     const { email, nombre, password, rol } = req.body;
     const hash = bcrypt.hashSync(password);
     const fecha = new Date();
@@ -37,7 +38,46 @@ exports.register = asyncHandler(async (req, res, next) => {
             token
         })
     }else{
-        res.status(400).json({ success: false,
-        err: 'email invalido' })
+        return next(new ErrorResponse('Por favor ingresa un email valido', 400))
     }
+})
+
+
+//  @Descripcion    Inicio de sesion
+//  @Ruta y Metodo  POST api/v1/auth/login
+//  @Acceso         Publica
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    const getSignedJwtToken = (user) => {
+        return jwt.sign({id: user},
+            process.env.JWT_SECRET
+        )
+    }
+
+    // Validando email y password
+    if(!email || !password){
+        return next(new ErrorResponse('Por favor ingresa un correo y contraseña', 400))
+    }
+
+    // Verificando si el usuario existe
+    const user = await db.select().from('usuarios').where({ email })
+
+    if(!user){
+        return next(new ErrorResponse('Credenciales invalidas', 401))
+    }
+
+    // Comparando las contraseñas
+    const isValid = bcrypt.compareSync(password, user[0].password);
+
+    if(!isValid){
+        return next(new ErrorResponse('Credenciales invalidas', 401))
+    }
+        const token = getSignedJwtToken(user);
+
+        res.status(201).json({
+            success: true,
+            user: user,
+            token
+        })
+
 })
