0242f04d0ac75c7b60b6ee2546694c343e05d458
Radames Vaz <rgvtovar@hotmail.com>
2020-10-06 16:06:15 -0400

    agregando modificacion de perfil del usuario ingresado

--- controllers/freelancers.js
+++ controllers/freelancers.js
@@ -3,21 +3,21 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
 
-//  @Descripcion    Busca todos los usuarios registrados
-//  @Ruta y Metodo  GET api/v1/usuarios
+//  @Descripcion    Busca todos los freelancers registrados
+//  @Ruta y Metodo  GET api/v1/freelancers
 //  @Acceso         Privada
-exports.buscarFreelancers = async (req, res, next) => {
+exports.buscarFreelancers = asyncHandler( async (req, res, next) => {
     const freelancers = await db('freelancers').select()
     res.status(200).json({
         success: true,
         data: freelancers
     })
-}
+})
 
-//  @Descripcion    Busca un solo usuario registrado
-//  @Ruta y Metodo  GET api/v1/usuarios/:id
+//  @Descripcion    Busca un solo freelancers registrado
+//  @Ruta y Metodo  GET api/v1/freelancers/:id
 //  @Acceso         Privada
-exports.buscarFreelance = async (req, res, next) => {
+exports.buscarFreelance = asyncHandler( async (req, res, next) => {
     const { id } = req.params;
     const freelancer = await db('freelancers').select().where({ id });
 
@@ -29,4 +29,27 @@ exports.buscarFreelance = async (req, res, next) => {
     } else {
         return next(new ErrorResponse(`El usuario con el id ingresado no existe`, 404))
     }
-}
+})
+
+//  @Descripcion    Crear el perfil del freelancer
+//  @Ruta y Metodo  PUT api/v1/freelancers/actualizarperfil
+//  @Acceso         Privada
+exports.modificarPerfil = asyncHandler (async (req, res, next) => {
+    const id = req.user[0].id;
+    const { categoria, descripcion, telefono, pais, habilidades } = req.body;
+
+        await db('freelancers').where({ id }).update({
+        categoria,
+        descripcion,
+        habilidades,
+        telefono,
+        pais
+    })
+    
+    const usuario = await db('freelancers').select().where({ id });
+
+    res.status(200).json({
+        success: true,
+        data: usuario
+    })
+})
--- routes/freelancers.js
+++ routes/freelancers.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const {
     buscarFreelancers,
-    buscarFreelance
+    buscarFreelance,
+    modificarPerfil
 } = require('../controllers/freelancers');
+
+//  Middleware
+const { protect } = require('../middleware/auth');
 
 router.route('/')
     .get(buscarFreelancers)
 
 router.route('/:id')
     .get(buscarFreelance)
+
+router.put('/actualizarperfil', protect, modificarPerfil)
 
 module.exports = router;
